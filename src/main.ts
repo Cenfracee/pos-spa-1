cd873de901977706bce6c390c29ef27571d58405
Sathimantha Malalaskera <sathimantha@gmail.com>
2021-01-25 01:43:52 +0530

    Initial commit. Webpack setup

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import './app/app.module';
+import style from './style.scss';
+import '../node_modules/admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js';
+import '../node_modules/admin-lte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js';
+import '../node_modules/admin-lte/dist/js/adminlte.js';
+
+var html = '<style>' + style + '</style>';
+$("head").append(html);
