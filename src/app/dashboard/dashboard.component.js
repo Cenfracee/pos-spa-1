aad18279d9a97e39041e49382106d268b660c28e
Sathimantha Malalaskera <sathimantha@gmail.com>
2021-01-25 02:00:11 +0530

    Dashboard content

---
+++ src/app/dashboard/dashboard.component.js
@@ -0,0 +1,6 @@
+import dashboard from './dashboard.component.html';
+import style from './dashboard.component.scss';
+
+$("app-dashboard").replaceWith('<div id="dashboard">' + dashboard + '</div>');
+var html = '<style>' + style + '</style>';
+$("#dashboard").append(html);
